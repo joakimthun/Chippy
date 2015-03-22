cfca029f0351ed83abe34737587b747e4f690c1d
joakimthun <joakimthun@gmail.com>
2015-03-22 13:22:28 +0100

    added some simple rendering

--- src/chippy.c
+++ src/chippy.c
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "chip8.h"
 
@@ -7,6 +8,7 @@
 int initializeSDL(int screen_width, int screen_height);
 void destroySDL();
 void renderDisplayBuffer(Chip8* chip);
+void putPixel(int x, int y, Uint32 pixel);
 
 SDL_Window* window = NULL;
 SDL_Surface* window_surface = NULL;
@@ -18,10 +20,6 @@ int main(int argc, char* args[])
 	{
 		return 1;
 	}
-
-	window_surface = SDL_GetWindowSurface(window);
-	SDL_FillRect(window_surface, NULL, SDL_MapRGB(window_surface->format, 0xFF, 0xFF, 0xFF));
-	SDL_UpdateWindowSurface(window);
 
 	char* file = "roms//pong2.c8";
 
@@ -38,8 +36,14 @@ int main(int argc, char* args[])
 
 	while (1) 
 	{
-		// Add something that slows this down
 		emulate(chip);
+		if (chip->redraw > 0)
+		{
+			renderDisplayBuffer(chip);
+			SDL_UpdateWindowSurface(window);
+		}
+
+		// Add something that slows this down
 	}
 
 	destroyChip(chip);
@@ -50,7 +54,27 @@ int main(int argc, char* args[])
 
 void renderDisplayBuffer(Chip8* chip)
 {
+	for (int i = 0; i < 2048; i++)
+	{
+		Uint32 color;
+		if (chip->display_buffer[i] == 0)
+			color = SDL_MapRGB(window_surface->format, 0, 0, 0);
+		else
+			color = SDL_MapRGB(window_surface->format, 255, 255, 255);
 
+		int x = i % 64;
+		int y = floor(i / 64);
+
+		// x, y , w, h
+		SDL_Rect fillRect = { x * 10, y * 10, 10, 10 };
+		SDL_FillRect(window_surface, &fillRect, color);
+	}
+}
+
+void putPixel(int x, int y, Uint32 pixel_value)
+{
+	Uint32 *pixels = (Uint32*)window_surface->pixels;
+	pixels[(y * window_surface->w) + x] = pixel_value;
 }
 
 int initializeSDL(int screen_width, int screen_height)
@@ -70,13 +94,17 @@ int initializeSDL(int screen_width, int screen_height)
 		}
 	}
 
+	window_surface = SDL_GetWindowSurface(window);
+	if (window_surface == NULL)
+	{
+		printf("A window surface could not be created! SDL_Error: %s\n", SDL_GetError());
+	}
+
 	return 0;
 }
 
 void destroySDL()
 {
-	SDL_FreeSurface(render_surface);
-	SDL_FreeSurface(window_surface);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
